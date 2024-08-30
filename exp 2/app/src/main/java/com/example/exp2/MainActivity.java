a6f9ca6dc5276b86e62acffad931beebe7f17535
dhee0901 <dheerajtrl007@gmail.com>
2024-08-30 18:59:04 +0530

    Add files via upload

---
+++ exp 2/app/src/main/java/com/example/exp2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exp2;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showmsg();
+            }
+        });
+    }
+    private void showmsg(){
+        Toast.makeText(this,"MESSAGE POPED UP",Toast.LENGTH_SHORT).show();
+    }
+}
